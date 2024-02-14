27db04348230f9f4ee961a7946a191e5f61b3cee
Mark Belanger <mbelanger@olin.edu>
2024-02-14 15:16:19 -0500

    Add working pico I2C and data savings scripts
    Reader and sender scripts modified to send to the correct address
    and accounts for the two bytes of data being sent. sensing_processor
    now successsfully writes data actively and names the csv file
    according to the date and time the script is run.

--- code/microcontroller/chamber_sensing_reader/chamber_sensing_reader.ino
+++ code/microcontroller/chamber_sensing_reader/chamber_sensing_reader.ino
@@ -1,22 +1,23 @@
 #include <Wire.h>
 
-uint8_t PRIMARY_ADDR = 8;
+uint8_t COMMS_ADDR = 0x10;
 uint16_t sensor_out;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  Wire.begin(PRIMARY_ADDR);
+  Wire.begin(COMMS_ADDR);
   Wire.onReceive(receiveEvent);
 }
 
-void receiveEvent() {
-  sensor_out = Wire.read();
-  // Math to convert raw output to force reading
-  Serial.println(sensor_out);
+void receiveEvent(int numBytes) {
+  if (numBytes >= 2) {                // Ensure at least 2 bytes are available
+    uint8_t msb = Wire.read();        // Most significant byte
+    uint8_t lsb = Wire.read();        // Least significant byte
+    sensor_out = (msb << 8) | lsb;    // Combine the two bytes into a uint16_t value
+    Serial.println(sensor_out);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
--- code/sensing_processor.py
+++ code/sensing_processor.py
@@ -1,16 +1,21 @@
 import serial
 import pandas as pd
 from time import time, ctime
+from datetime import datetime
 
-# Set time in seconds (for file naming)
+
+# Set time in unit seconds
 t = time()
 
-# convert to local time
-local_time = ctime(t)
+# Convert time from unit seconds to a date + time
+local_time = datetime.fromtimestamp(t)
+
+# Convert the date + time into a format that can be used in a file name
+formatted_time = local_time.strftime("%Y-%m-%d_%H-%M-%S")
 
 
 # Define the COM port the microcontroller is connected to
-CONTROLLER_COMM_PORT = ""
+CONTROLLER_COMM_PORT = "COM5"
 
 # Set baud rate
 BAUD_RATE = 9600
@@ -18,16 +23,16 @@ BAUD_RATE = 9600
 # Open the COM port to start communications
 serial_port = serial.Serial(CONTROLLER_COMM_PORT, BAUD_RATE, timeout=1)
 
+with open(f"Trial {formatted_time}.csv", "w") as data:
+    while True:
+        sensor_output = int(serial_port.readline().decode())
+        # print(sensor_output)
+        # print(type(sensor_output))
 
-while True:
-    sensor_output = serial_port.readline().decode()
-
-    if len(sensor_output > 0):
-        # force, temp_1, temp_2, etc = (int(sensor) for sensor in sensor_output.split(","))
-        force = sensor_output
-
-        with open(f"Trial {local_time}.csv", "wb") as data:
+        if sensor_output > 0:
+            # force, temp_1, temp_2, etc = (int(sensor) for sensor in sensor_output.split(","))
+            force = sensor_output
             data.write(ctime(time()))
             data.write(",")
-            data.write(force)
-            data.write("\n")
+            data.write(str(force))
+            data.write(",\n")
