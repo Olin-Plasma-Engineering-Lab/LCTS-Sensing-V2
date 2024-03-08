798c820332f8dcb4d2c56038d39a204e0dd9cc8e
Mark Belanger <mbelanger@olin.edu>
2024-03-08 08:21:44 -0500

    Use two I2C channels for two way comms

--- code/microcontroller/chamber_sensing_reader/chamber_sensing_reader.ino
+++ code/microcontroller/chamber_sensing_reader/chamber_sensing_reader.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 
-uint8_t COMMS_ADDR = 0x10;
+int const COMMS_ADDR = 0x10;
+int const CAL_ADDR = 0x20;
 uint16_t sensor_out;
 
 void setup() {
@@ -9,8 +10,9 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
   Wire.begin(COMMS_ADDR);
   Wire.onReceive(receiveEvent);
-  //Wire.onRequest(requestEvent);
-  
+  Wire1.setSDA(4);
+  Wire1.setSCL(5);
+  Wire1.begin(); 
 }
 
 void receiveEvent(int numBytes) {
@@ -22,62 +24,19 @@ void receiveEvent(int numBytes) {
   }
 }
 
-void requestEvent() {
-      if (Serial.available()) {
+
+void loop() {
+if (Serial.available()) {
       String data = Serial.readString();
       Serial.println(data);
       if (data == "up\n") {
-        Wire.write(2);  // send value 2
+        Wire1.write(2);  // send value 2
         Serial.println("up sent");
       }
       if (data == "down\n") {
-        Wire.write(1);  // send value 1
+        Wire1.write(1);  // send value 1
         Serial.println("down sent");
       }
   }
-}
-
-// void requestEvent() {
-    // if (Serial.available() > 0) {
-  //       String direction = Serial.readString(); 
-  //       Serial.println(direction);
-        
-  //       switch (direction) {
-  //           case "u\n":
-  //               Wire.write(2);
-  //               Serial.println("up sent");  // for debugging
-  //               break;
-  //           case "d\n":
-  //               Wire.write(1); 
-  //               Serial.println("down sent"); // for debugging
-  //               break;
-  //           default:
-  //               Serial.println("Invalid direction");
-  //       }
-  //   }  
-// }
-
-
-
-void loop() {
-//  if (Serial.available()) {
-//     String data = Serial.readString();
-//     Serial.println(data);
-//     Serial.println (data == "up\n");
-//   } 
-
-
-    // if (Serial.available()) {
-    // String data = Serial.readString();
-    // Serial.println(data);
-    // if (data == "up\n") {
-    //   Wire.write(2);  // send value 2
-    //   Serial.println("up sent");
-    // }
-    // if (data == "down\n") {
-    //   Wire.write(1);  // send value 1
-    //   Serial.println("down sent");
-    // }
-    // }
 }
   
--- code/microcontroller/chamber_sensing_sender/chamber_sensing_sender.ino
+++ code/microcontroller/chamber_sensing_sender/chamber_sensing_sender.ino
@@ -1,25 +1,45 @@
 #include <Wire.h>
 #include "Stepper.h"
 
-
-int const  COMMS_ADDR = 0x10;
-uint16_t   force;
-uint8_t    FORCE_SENSOR = A0;
+int const COMMS_ADDR = 0x10;
+int const CAL_ADDR = 0x20;
+uint16_t force;
+uint8_t FORCE_SENSOR = A0;
 const int REVOLUTION_STEPS = 200;
 uint16_t step = 10;
 
 Stepper calibration(REVOLUTION_STEPS, 9, 10, 11, 12);
 
-void setup() {
+void setup()
+{
   calibration.setSpeed(60);
   pinMode(FORCE_SENSOR, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(9600);
   Wire.begin();
+  Wire1.setSDA(4);
+  Wire1.setSCL(5);
+  Wire1.begin(CAL_ADDR);
+  Wire1.onReceive(receiveEvent);
+
 }
 
-void loop() {
+void receiveEvent(int numBytes) {
+  byte direction = Wire.read();
+  if (direction == 2)
+  {
+    calibration.step(step);
+  }
+  else if (direction == 1)
+  {
+    calibration.step(-step);
+  }
+  Serial.println(direction);
+}
+
+void loop()
+{
   delay(50);
   force = analogRead(FORCE_SENSOR);
 
@@ -28,35 +48,17 @@ void loop() {
   uint8_t lsb = force & 0xFF;
 
   Wire.beginTransmission(COMMS_ADDR);
-  Wire.write(msb);                      // Send the most significant byte first
-  Wire.write(lsb);                      // Send the least significant byte
+  Wire.write(msb); // Send the most significant byte first
+  Wire.write(lsb); // Send the least significant byte
   int result = Wire.endTransmission();
 
   // Debugging (if need be)
 
-  if (result != 0) {
+  if (result != 0)
+  {
     Serial.print("Error in I2C transmission: ");
     Serial.println(result);
   }
 
-    Wire.requestFrom(COMMS_ADDR, 1);
-  //  if(Wire.available()){
-  //   Serial.println("RC");
-  //  }
-    // byte direction = Wire.read();
-    // if (direction == 2) {
-    //   calibration.step(step);
-    // } else if (direction == 1) {
-    //   calibration.step(-step);
-    // }
-    // Serial.println(direction);
-    
-  
-
-  //Serial.println(force);
-
-  }
-
-
-
-
+  // Serial.println(force);
+}
